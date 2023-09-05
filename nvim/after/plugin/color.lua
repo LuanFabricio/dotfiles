10253322df907f6b48bbdccfcee5f3437906264f
LuanFabricio <luanlimaleite@hotmail.com>
2023-09-05 01:29:19 -0300

    atualizando dotfiles

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -4,9 +4,9 @@ function Color(color)
 	vim.cmd.colorscheme(color)
 end
 
--- Color("torte")
+Color("torte")
 -- Color("oxocarbon")
 -- Color("kanagawa-dragon")
 -- Color("kanagawa-wave")
 -- Color("catppuccin-macchiato");
-Color("industry")
+-- Color("industry")
