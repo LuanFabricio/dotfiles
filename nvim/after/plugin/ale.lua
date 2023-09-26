ced14eb85dedddb0f4f4d636cbcf28661491c6fb
LuanFabricio <luanlimaleite@hotmail.com>
2023-09-25 21:58:04 -0300

    desligando o ALE em linguagens que não tem linter configurado

--- nvim/after/plugin/ale.lua
+++ nvim/after/plugin/ale.lua
@@ -14,3 +14,4 @@ augroup END")
 vim.cmd("let g:ale_fixers = {'javascript': ['prettier', 'eslint'], 'jsx': ['prettier', 'eslint'], 'typescript': ['prettier', 'eslint'], 'tsx': ['prettier', 'eslint'], '*': ['remove_trailing_lines', 'trim_whitespace']}")
 
 vim.cmd("let g:ale_fix_on_save = 1")
+vim.cmd("let g:ale_linters_explicit = 1")
