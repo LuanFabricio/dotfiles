028b6900a3d38dbdb72bd19430a04e25a90e4cad
LuanFabricio <luanlimaleite@hotmail.com>
2024-05-11 13:16:35 -0300

    Updating the default color

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -4,11 +4,12 @@ function Color(color)
 	vim.cmd.colorscheme(color)
 end
 
+Color()
+
 -- Color("slate")
 -- Color("torte")
 -- Color("oxocarbon")
 -- Color("kanagawa-dragon")
 -- Color("kanagawa-wave")
-Color("catppuccin-mocha");
 -- Color("catppuccin-macchiato");
 -- Color("industry")
