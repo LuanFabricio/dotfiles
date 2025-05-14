25b7d931d2b7020d71f45110615c6c1f67f9299d
Luan Fabrício <luanlimaleite@hotmail.com>
2025-05-13 23:23:40 -0300

    Adds new programs on install script

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ sudo pacman -S gnome-terminal chromium wlsunset tmux neovim vim git curl ninja c
 
 # games
 # enable multilib https://wiki.archlinux.org/title/Official_repositories#multilib
-sudo pacman -s steam lutris wine
+sudo pacman -S steam lutris wine winetricks
 
 # utils tools
-sudo pacman -S evince nautilus obs vlc gimp
+sudo pacman -S evince nautilus obs vlc gimp rofi
 
 # others
-sudo pacman -S discord flameshot filezilla
+sudo pacman -S discord flameshot filezilla rim slurp wl-clipboard
 
 # Lazy vim
 git clone https://github.com/LazyVim/starter ~/.config/nvim
