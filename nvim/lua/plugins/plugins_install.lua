123063ccf4aa681a9df0e371a5fcadb08bc88257
Luan Fabrício <luanlimaleite@hotmail.com>
2026-03-24 19:09:32 -0300

    Updates plugins

--- nvim/lua/plugins/plugins_install.lua
+++ nvim/lua/plugins/plugins_install.lua
@@ -43,8 +43,16 @@ return {
 	},
 
 	{'nyoom-engineering/oxocarbon.nvim'},
-	{'dart-lang/dart-vim-plugin'},
-	{'akinsho/flutter-tools.nvim', dependencies = 'nvim-lua/plenary.nvim'},
+	--{'dart-lang/dart-vim-plugin'},
+	{
+	    'nvim-flutter/flutter-tools.nvim',
+	    lazy = false,
+	    dependencies = {
+		'nvim-lua/plenary.nvim',
+		'stevearc/dressing.nvim', -- optional for vim.ui.select
+	    },
+	    config = true,
+	},
 	{'nvim-tree/nvim-web-devicons'},
 	{'gpanders/editorconfig.nvim'},
 	{
@@ -66,29 +74,4 @@ return {
 	{ "catppuccin/nvim", as = "catppuccin" },
 	{'lervag/vimtex'},
 	{ "EdenEast/nightfox.nvim" },
-	{
-	  "scalameta/nvim-metals",
-	  dependencies = {
-	    "nvim-lua/plenary.nvim",
-	  },
-	  ft = { "scala", "sbt", "java" },
-	  opts = function()
-	    local metals_config = require("metals").bare_config()
-	    metals_config.on_attach = function(client, bufnr)
-	      -- your on_attach function
-	    end
-
-	    return metals_config
-	  end,
-	  config = function(self, metals_config)
-	    local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-	    vim.api.nvim_create_autocmd("FileType", {
-	      pattern = self.ft,
-	      callback = function()
-		require("metals").initialize_or_attach(metals_config)
-	      end,
-	      group = nvim_metals_group,
-	    })
-	  end
-	}
 }
